81de09ba82b6356cb491e190f69044e7502014c3
programmatoroSeduto <francesc.ganci@gmail.com>
2021-06-24 22:41:58 +0000

    first stable version

--- final_assignment/scripts/user_console.py
+++ final_assignment/scripts/user_console.py
@@ -1,10 +1,13 @@
 #! /usr/bin/env python 
 
+
+
 import rospy
 from std_srvs.srv import SetBool
 from final_assignment.srv import switch_service, switch_serviceRequest, switch_serviceResponse
 from final_assignment.srv import user_target, user_targetRequest, user_targetResponse
 from final_assignment.srv import position_defined, position_definedRequest, position_definedResponse
+from final_assignment.srv import bug0_status, bug0_statusRequest, bug0_statusResponse
 from geometry_msgs.msg import Point
 
 
@@ -24,6 +27,12 @@ name_user_target = "/user_target"
 
 ## name of the service 'position_defined'
 name_position_defined = "/position_defined"
+
+## name of the service 'bug0_switch'
+name_bug0_switch = "/bug0_switch"
+
+## name of the service 'bug0_status'
+name_bug0_status = "/bug0_status"
 
 ## available commands
 commands = [ "", \
@@ -57,19 +66,23 @@ reach_random_pos_active = False
 ## is wall follow active?
 wall_follow_active = False
 
+## use bug0 or move_base
+use_bug0 = False
+
 
 
 
 
 # --------------------------------- FUNCTIONS
 
-def reach_random_pos():
+def reach_random_pos( ):
 	''' activate the node 'reach_random_pos_service.py'.
-		\TODO integration with bug0
+	
 	'''
-	global node_name
+	global node_name, use_bug0
 	global reach_random_pos_active, robot_busy
 	global name_reach_random_pos_switch, srv_reach_random_pos_switch
+	global name_bug0_switch, srv_bug0_switch
 	
 	if reach_random_pos_active:
 		# the service is already active
@@ -81,14 +94,17 @@ def reach_random_pos():
 			rospy.logwarn( " [%s] ATTENTION: the robot is busy! ", node_name )
 			print( "\tPlease turn off the previous command (use last_pos) before calling this one. " )
 		else:
-			# activate the service 'reach_random_pos_service'
-			srv_reach_random_pos_switch( True )
+			# activate the service
+			if not use_bug0:
+				srv_reach_random_pos_switch( True, False )
+			else:
+				srv_bug0_switch( True, False )
 			
 			# set the robot as busy
 			robot_busy = True
 			reach_random_pos_active = True
 			
-			rospy.loginfo( " [%s] reach_random_pos STARTED. ", node_name )
+			rospy.loginfo( " [%s] reach_random_pos STARTED using '%s'", node_name, ( "bug0" if use_bug0 else "move_base" ) )
 
 
 
@@ -96,14 +112,21 @@ def stop_reach_random_pos( ):
 	'''
 		stop the activity 'reach_random_pos'
 	'''
+	global use_bug0
 	global robot_busy, reach_random_pos_active
+	global name_bug0_switch, srv_bug0_switch
+	global name_reach_random_pos_switch, srv_reach_random_pos_switch
 	
 	# send the stop signal to the node
 	rospy.loginfo( " [%s] sending the stop signal... ", node_name )
-	res = srv_reach_random_pos_switch( False )
+	res = None
+	if not use_bug0:
+		res = srv_reach_random_pos_switch( False, False )
+	else:
+		res = srv_bug0_switch( False, False )
 	
 	# then wait for the robot to reach the final position
-	if res.success:
+	if res.success or use_bug0:
 		# immediate end of the activity
 		pass
 	else:
@@ -111,7 +134,7 @@ def stop_reach_random_pos( ):
 		rospy.loginfo( " [%s] waiting for the robot to end the path... ", node_name )
 		while res.in_progress:
 			rospy.sleep( rospy.Duration( 0, 500 ) )
-			res = srv_reach_random_pos_switch( False )
+			res = srv_reach_random_pos_switch( False, False )
 	
 	robot_busy = False
 	reach_random_pos_active = False
@@ -128,6 +151,8 @@ def reach_user_pos():
 	global name_position_defined, srv_position_defined
 	global name_user_target, srv_user_target
 	global robot_busy
+	global name_bug0_switch, srv_bug0_switch
+	global name_bug0_status, srv_bug0_status
 	
 	if robot_busy:
 		rospy.logwarn( " [%s] ATTENTION: the robot is busy now! ", node_name )
@@ -145,10 +170,42 @@ def reach_user_pos():
 	res = srv_position_defined( target )
 	if res.defined :
 		rospy.loginfo( " [user console] selected: ( %f, %f )", target.x, target.y )
-		# reach_this( target )
-		srv_user_target( target )
 	else:
 		rospy.logerr( " [user console] ERROR: not a valid position. " )
+		return
+	
+	# reach the position
+	rospy.loginfo( " [%s] reaching the position... ", node_name )
+	if not use_bug0:
+		srv_user_target( target )
+	else:
+		reach_user_pos_bug0( target )
+	
+	rospy.loginfo( " [%s] Arrived. ", node_name )
+
+
+
+def reach_user_pos_bug0( target ):
+	''' reach a given target using bug0 algorithm
+		\param target (geometry_msgs/Point)
+	'''
+	global node_name
+	global name_user_target, srv_user_target
+	global name_bug0_switch, srv_bug0_switch
+	global name_bug0_status, srv_bug0_status
+	
+	# first of all, put the target into the parameter server
+	rospy.set_param("des_pos_x", target.x)
+	rospy.set_param("des_pos_y", target.y)
+	
+	# activate bug0 in once_only mode
+	res = srv_bug0_switch( True, only_once=True )
+	
+	# wait until the robot has reached the goal
+	goal_reached = ( srv_bug0_status( ) ).reached
+	while not goal_reached:
+		rospy.sleep( rospy.Duration( 0, 500 ) )
+		goal_reached = ( srv_bug0_status( ) ).reached
 
 
 
@@ -203,8 +260,27 @@ def change_motion_planning_algorithm():
 	'''select a motion planning algorithm.
 
 	'''
-	global node_name
-	pass
+	global node_name, use_bug0, robot_busy
+	
+	# you can't change the motion planning algorithm while you're using it!
+	if robot_busy:
+		rospy.logwarn( " [%s] ATTENTION: the robot is busy now! ", node_name )
+		print( "\tPlease turn off the previous command (use last_pos) before calling this one. " )
+		
+		return 
+	
+	rospy.loginfo( "[%s] actually you're using this motion planning algorithm: '%s' ", node_name, ('bug0' if use_bug0 else 'move_base') )
+	print( "[%s] would you like to change with this? '%s' [Y/n]" % ( node_name, ('bug0' if not use_bug0 else 'move_base') ) )
+	answer = input( "[Y/n]" )
+	
+	if answer == "Y" or answer == "y":
+		# change planning algorithm
+		use_bug0 = not use_bug0
+	else:
+		# don't change algorithm
+		pass
+	
+	rospy.loginfo( "[%s] using algorithm: '%s' ", node_name, ('bug0' if use_bug0 else 'move_base') )
 
 
 
@@ -236,6 +312,12 @@ srv_user_target = None
 
 ## call-point of the service 'position_defined'
 srv_position_defined = None
+
+## call-point of the service 'bug0_switch'
+srv_bug0_switch = None
+
+## call-point of the service 'bug0_status'
+srv_bug0_status = None
 
 
 
@@ -350,6 +432,18 @@ if __name__ == "__main__":
 	rospy.wait_for_service( name_position_defined )
 	srv_position_defined = rospy.ServiceProxy( name_position_defined, position_defined )
 	rospy.loginfo( " [%s] service %s ... OK", node_name, name_position_defined )
+	
+	# service 'bug0_switch'
+	rospy.loginfo( " [%s] getting service %s ...", node_name, name_bug0_switch )
+	rospy.wait_for_service( name_bug0_switch )
+	srv_bug0_switch = rospy.ServiceProxy( name_bug0_switch, switch_service )
+	rospy.loginfo( " [%s] service %s ... OK", node_name, name_bug0_switch )
+	
+	# service 'bug0_status'
+	rospy.loginfo( " [%s] getting service %s ...", node_name, name_bug0_status )
+	rospy.wait_for_service( name_bug0_status )
+	srv_bug0_status = rospy.ServiceProxy( name_bug0_status, bug0_status )
+	rospy.loginfo( " [%s] service %s ... OK", node_name, name_bug0_status )
 	
 	try:
 		rospy.loginfo( " [%s] is ONLINE", node_name )
